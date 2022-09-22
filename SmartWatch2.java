19fa66353acc08c47152627e38c05ac9b311bc4f
sangeetha63 <sangeethahj.xworkz@gmail.com>
2022-09-22 12:35:28 +0530

    committing arrays related files

---
+++ SmartWatch2.java
@@ -0,0 +1,21 @@
+class SmartWatch2{
+   public static void showTime(){
+	   System.out.println("11:00Am");
+	   return;
+   }
+	   public static void showHeartBeats(){
+	   System.out.println("72 bpm");
+	   return;
+	   }
+	   public static void showNotification(){
+	   System.out.println("Call From Appa");
+	   return;
+   }
+   
+  	
+	  
+	  }
+
+
+
+
---
+++ SmartWatch2Tester.java
@@ -0,0 +1,15 @@
+class SmartWatch2Tester{
+   
+   public static void main(String smart[]){
+	   
+	 SmartWatch2.showTime();
+	 SmartWatch2.showHeartBeats();
+	 SmartWatch2.showNotification();
+	  
+	 
+	  
+	  }
+}
+
+
+
