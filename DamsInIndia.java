7b236418efa1d8fda07f758625fdcf5de2fc2501
sangeetha63 <sangeethahj.xworkz@gmail.com>
2022-12-23 11:09:21 +0530

    files

---
+++ DamsInIndia.java
@@ -0,0 +1,293 @@
+package com.xworkz.collectionapp;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+public class DamsInIndia {
+	public static void main(String[] args) {
+		
+		Collection<String>damInKarntaka=new ArrayList<String>();
+		damInKarntaka.add("KRS");
+		damInKarntaka.add("TB");
+		damInKarntaka.add("Suvarnavathi");
+		damInKarntaka.add("Gajanur");
+		damInKarntaka.add("Tungabhadra");
+		System.out.println("size of dam in karnataka+"+damInKarntaka);
+		
+		Collection<String>damInTamilNadu=new ArrayList<String>();
+		damInTamilNadu.add("Aliyar");
+		damInTamilNadu.add("Mettur");
+		damInTamilNadu.add("Nirar");
+		System.out.println("size of dam in taminadu+"+damInTamilNadu);
+		
+		Collection<String>damInAndraPradesh=new ArrayList<String>();
+		damInAndraPradesh.add("Polavaram");
+		damInAndraPradesh.add("PABR");
+		damInAndraPradesh.add("Jalaput");
+		System.out.println("size of dam in Andrapradesh+"+damInAndraPradesh);
+		
+		Collection<String>damInArunachalPradesh=new ArrayList<String>();
+		damInArunachalPradesh.add("Ranganadhi");
+		damInArunachalPradesh.add("Dibang");
+		System.out.println("size of dam in Arunachalpradesh+"+damInArunachalPradesh);
+		
+		Collection<String>damInChhatisgarh=new ArrayList<String>();
+		damInChhatisgarh.add("Dudhawa");
+		damInChhatisgarh.add("Sondur");
+		System.out.println("size of dam in Chhatisgarh+"+damInChhatisgarh);
+		
+		Collection<String>damInBihar=new ArrayList<String>();
+		damInBihar.add("Dudhawa");
+		damInBihar.add("Sondur");
+		System.out.println("size of dam in Bihar+"+damInBihar);
+		
+		Collection<String>damInGoa=new ArrayList<String>();
+		damInGoa.add("Amtane");
+		damInGoa.add("Chapoli");
+		System.out.println("size of dam in goa+"+damInGoa);
+		
+		Collection<String>damInGujarat=new ArrayList<String>();
+		damInGujarat.add("hemil");
+		damInGujarat.add("mitti");
+		System.out.println("size of dam in gujarat+"+damInGujarat);
+		
+		Collection<String>damInHaryana=new ArrayList<String>();
+		damInHaryana.add("pallobaragge");
+		damInHaryana.add("ottu");
+		System.out.println("size of dam in Haryana+"+damInHaryana);
+		
+		Collection<String>damInHimachalPradesh=new ArrayList<String>();
+		damInHimachalPradesh.add("Bhakra");
+		damInHimachalPradesh.add("Pong");
+		damInHimachalPradesh.add("Kola");
+		System.out.println("size of dam in HimachalPradesh+"+damInHimachalPradesh);
+		
+		Collection<String>damInJammuAndKashmir=new ArrayList<String>();
+		damInJammuAndKashmir.add("saral");
+		System.out.println("size of dam in JammuAndKashmir+"+damInJammuAndKashmir);
+		
+		Collection<String>damInJharkhand=new ArrayList<String>();
+		damInJharkhand.add("Chandil");
+		damInJharkhand.add("Panchut");
+		System.out.println("size of dam in Jharkhand+"+damInJharkhand);
+		
+		Collection<String>damInKerla=new ArrayList<String>();
+		damInKerla.add("Kakki");
+		damInKerla.add("pamba");
+		System.out.println("size of dam in Kerla+"+damInKerla);
+		
+		Collection<String>damInMadhyaPradesh=new ArrayList<String>();
+		damInMadhyaPradesh.add("Bargi");
+		damInMadhyaPradesh.add("Barna");
+		damInMadhyaPradesh.add("Harshi");
+		damInMadhyaPradesh.add("GandiSagar");
+		System.out.println("size of dam in MadhyaPradesh+"+ damInMadhyaPradesh);
+		
+		Collection<String>damInManipur=new ArrayList<String>();
+		damInManipur.add("Khuga");
+		damInManipur.add("TipaiMukh");
+		System.out.println("size of dam in Manipur+"+damInManipur);
+		
+		Collection<String>damInMaharashtra=new ArrayList<String>();
+		damInMaharashtra.add("Dhamapur");
+		damInMaharashtra.add("Ashti");
+		System.out.println("size of dam in Maharashtra+"+ damInMaharashtra);
+		
+		Collection<String>damInMizoram=new ArrayList<String>();
+		damInMizoram.add("Serluli");
+		damInMizoram.add("Tuirial");
+		System.out.println("size of dam in Mizoram+"+ damInMizoram);
+		
+		Collection<String>damInOdisha=new ArrayList<String>();
+		damInOdisha.add("Hirakud");
+		damInOdisha.add("Mandira");
+		System.out.println("size of dam in Odisha+"+ damInOdisha);
+		
+		Collection<String>damInPunjab=new ArrayList<String>();
+		damInPunjab.add("RanjitSagar");
+		System.out.println("size of dam in Odisha+"+ damInPunjab);
+		
+		Collection<String>damInRajasthan=new ArrayList<String>();
+		damInRajasthan.add("Kota");
+		damInRajasthan.add("Jawahar Sagar");
+		damInRajasthan.add("Gandhi Sagar");
+		System.out.println("size of dam in Rajasthan+"+ damInRajasthan);
+		
+		Collection<String>damInSikkim=new ArrayList<String>();
+		damInSikkim.add("Rangit");
+		System.out.println("size of dam in Sikkim+"+ damInSikkim);
+		
+		Collection<String>damInTelagana=new ArrayList<String>();
+		damInTelagana.add("Nizam sagar");
+		damInTelagana.add("Shiram sagar");
+		System.out.println("size of dam in Telagana+"+ damInTelagana);
+		
+		Collection<String>damInUttarkhand=new ArrayList<String>();
+		damInUttarkhand.add("Khari");
+		damInUttarkhand.add("Maneri");
+		System.out.println("size of dam in Uttarkhand+"+ damInUttarkhand);
+		
+		Collection<String>damInWestBengal=new ArrayList<String>();
+		damInWestBengal.add("Durgapur");
+		damInWestBengal.add("Panchut");
+		System.out.println("size of dam in WestBengal+"+ damInWestBengal);
+		
+		Collection<String>damInMeghalaya=new ArrayList<String>();
+		damInMeghalaya.add("Khandang");
+		damInMeghalaya.add("Mawphlang");
+		System.out.println("size of dam in Meghalaya+"+ damInMeghalaya);
+		
+		Collection<String>damInAssam=new ArrayList<String>();
+		damInAssam.add("Pagladia");
+		damInAssam.add("Umrong");
+		System.out.println("size of dam in Assam+"+ damInAssam);
+		
+		Collection<String>damInNagaland=new ArrayList<String>();
+		damInNagaland.add("Doyang");
+		System.out.println("size of dam in Nagaland+"+ damInNagaland);
+		
+		Collection<String>damInTripura=new ArrayList<String>();
+		damInTripura.add("Amarpur");
+		System.out.println("size of dam in Tripura+"+ damInTripura);
+		
+		Collection<String>damInUttarPradesh=new ArrayList<String>();
+		damInUttarPradesh.add("Adwa");
+		System.out.println("size of dam in UttarPradesh+"+ damInUttarPradesh);
+		
+		Collection<String>damInIndia=new ArrayList<String>();
+		damInIndia.addAll(damInAndraPradesh);
+		damInIndia.addAll(damInArunachalPradesh);
+		damInIndia.addAll(damInAssam);
+		damInIndia.addAll(damInBihar);
+		damInIndia.addAll(damInChhatisgarh);
+		damInIndia.addAll(damInGoa);
+		damInIndia.addAll(damInGujarat);
+		damInIndia.addAll(damInHaryana);
+		damInIndia.addAll(damInHimachalPradesh);
+		damInIndia.addAll(damInJammuAndKashmir);
+		damInIndia.addAll(damInJharkhand);
+		damInIndia.addAll(damInKarntaka);
+		damInIndia.addAll(damInKerla);
+		damInIndia.addAll(damInMadhyaPradesh);
+		damInIndia.addAll(damInMaharashtra);
+		damInIndia.addAll(damInManipur);
+		damInIndia.addAll(damInMeghalaya);
+		damInIndia.addAll(damInMizoram);
+		damInIndia.addAll(damInNagaland);
+		damInIndia.addAll(damInOdisha);
+		damInIndia.addAll(damInPunjab);
+		damInIndia.addAll(damInRajasthan);
+		damInIndia.addAll(damInSikkim);
+		damInIndia.addAll(damInTamilNadu);
+		damInIndia.addAll(damInTelagana);
+		damInIndia.addAll(damInTripura);
+		damInIndia.addAll(damInUttarPradesh);
+		damInIndia.addAll(damInUttarkhand);
+		damInIndia.addAll(damInWestBengal);
+		System.out.println("elements the states");
+		System.out.println(damInIndia.size());
+		
+		//starts with T
+		Iterator<String> dams=damInIndia.iterator();
+		while(dams.hasNext()) {
+			String element =dams.next();
+			if(element.startsWith("T")) {
+				System.out.println("elements starts with T:"+element);
+			}
+		}
+		//ends with ra
+			Iterator<String> iter=damInIndia.iterator();
+			while(iter.hasNext()) {
+				String elements =iter.next();
+				if(elements.endsWith("ra")) {
+					System.out.println("elements ends with ra:"+elements);
+				}
+		
+			}
+			//remove  all dam if contains p
+			 System.out.println("...remove element of p...");
+			 
+			 Iterator<String> itr=damInIndia.iterator();
+				while(itr.hasNext()) {
+					String elements =itr.next();
+					if(elements.contains("P")) {
+						System.out.println("elements contains P:"+elements);
+	}
+	}
+				
+				
+             //UpperCase
+				System.out.println("...Enter the UpperCase...");
+				
+				 Iterator<String> iterator=damInIndia.iterator();
+					while(iterator.hasNext()) {
+						String upper =iterator.next();
+							System.out.println(upper.toUpperCase());
+
+	}
+					
+					
+					//LowerCase
+					System.out.println("...Enter the LowerCase...");
+					
+					 Iterator<String> iterat=damInIndia.iterator();
+						while(iterat.hasNext()) {
+							String upper =iterat.next();
+								System.out.println(upper.toLowerCase());
+	}
+						
+						Iterator<String> palindrome = damInIndia.iterator();
+					    
+					    while(palindrome.hasNext())
+					    {
+					    	String string = palindrome.next();
+					    	int c = 0;
+					    	
+					    	StringBuffer buffering = new StringBuffer(string);
+					    	buffering.reverse();
+					    	
+					    	String pali = buffering.toString();
+					    	if(string.equals(pali))
+					    	{
+					    		c++;
+					    		System.out.println("found palindrome:"+buffering);
+					    	}
+					    }
+					       
+					    
+					    
+					    
+					    
+					    
+					    
+					    
+					    
+						}
+
+					}
+				
+						
+
+							
+
+
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	
+
+
