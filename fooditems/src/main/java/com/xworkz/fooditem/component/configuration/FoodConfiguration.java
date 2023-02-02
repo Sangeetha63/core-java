d14ec30a25c20c886f4d00708974bb3ce1a04040
sangeetha63 <sangeethahj.xworkz@gmail.com>
2023-02-02 08:21:28 +0530

    files

---
+++ fooditems/src/main/java/com/xworkz/fooditem/component/configuration/FoodConfiguration.java
@@ -0,0 +1,13 @@
+package com.xworkz.fooditem.component.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.xworkz.fooditem")
+public class FoodConfiguration {
+
+	public FoodConfiguration() {
+		System.out.println("Created" + this.getClass().getSimpleName());
+	}
+}
---
+++ fooditems/src/main/java/com/xworkz/fooditem/component/configuration/Foodinit.java
@@ -0,0 +1,38 @@
+package com.xworkz.fooditem.component.configuration;
+
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class Foodinit extends AbstractAnnotationConfigDispatcherServletInitializer implements WebMvcConfigurer{
+	public Foodinit() {
+		System.out.println("Created" + this.getClass().getSimpleName());
+	}
+	
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		System.out.println("Running getRootConfigClasses");
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		System.out.println("Running getServletConfigClasses");
+		return new Class[] {FoodConfiguration.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		System.out.println("Running getServletMappings");
+		return new String[] {"/"};
+	}
+
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		System.out.println("Running configureDefaultServletHandling");
+		configurer.enable();
+	}
+}
+
+
+
